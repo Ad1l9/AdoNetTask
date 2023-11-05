c5b2675592f6613c562570baaf142b8ecc78ebb5
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-11-06 01:45:09 +0400

    Add project files.

---
+++ AdoNetTask/Datas/Sql.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetTask.Datas
+{
+    public class Sql
+    {
+        private const string _connectionString = "Data Source=WINDOWS-DPJDVHL\\SQLEXPRESS;Initial Catalog=SqlHomeWork;Integrated Security=True;TrustServerCertificate=True";
+        private static SqlConnection _connection = new SqlConnection(_connectionString);
+
+        public int ExecuteCommand(string cmd)
+        {
+            int result = 0;
+            try
+            {
+                _connection.Open();
+
+                SqlCommand command = new SqlCommand(cmd, _connection);
+                result = command.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return result;
+
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                _connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+                adapter.Fill(table);
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+
+            return table;
+
+        }
+    }
+}
---
+++ AdoNetTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdoNetTask.Models;
+using AdoNetTask.Services;
+
+ArtistService artistService = new ArtistService();
+Artist artist = new Artist()
+{
+    Name = "Adil",
+    Surname = "Nasirli",
+    Birthday = DateTime.Now,
+    Gender = "Male"
+};
+//artistService.Create(artist);
+
+
+
+artistService.Delete(7);
+
+List<Artist> artists = artistService.GetAll();
+
+
+foreach (Artist item in artists)
+{
+    Console.WriteLine(item);
+}
+
+//Artist artist1 = artistService.GetById(2);
+//Console.WriteLine(artist);
+
+//MusicService ms = new MusicService();
+//Music music = new Music()
+//{
+//    Name = "Leyla",
+//    Duration = 1000,
+//    IsDeleted = false,
+//};
+//ms.Create(music);
+//ms.Delete(1);
+
+//List<Music> musics = ms.GetAll();
+
+
+//foreach (Music musicc in musics)
+//{
+//    Console.WriteLine(musicc);
+//}
